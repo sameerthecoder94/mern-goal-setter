d2595e1d469d135f77b7bdab016ff9af6ec865ec
sameerthecoder94 <sameerthecoder@gmail.com>
2022-02-17 16:19:25 -0500

    added middleware

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
+//body-parser middleware for post request
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/goals', require('./routes/goalRoutes'));
+
+//errorHandler middleware
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
