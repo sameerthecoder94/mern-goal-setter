9e6591c688fe433a3256d4c58eb576e58fee4a72
sameerthecoder94 <sameerthecoder@gmail.com>
2022-02-17 14:55:36 -0500

    created routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('Hello Server');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
