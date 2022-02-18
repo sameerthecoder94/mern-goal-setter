f20a1a06dd331b559e934aafe0b34e5a87c12665
sameerthecoder94 <sameerthecoder@gmail.com>
2022-02-18 10:33:54 -0500

    generate JWT token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,18 +12,15 @@ const resgisterUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('Please add all fields');
   }
-
   //check if user exists
   const userExists = await User.findOne({ email });
   if (userExists) {
     res.status(400);
     throw new Error('User already exists');
   }
-
   //hash password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
-
   //create user
   const user = await User.create({
     name,
@@ -35,6 +32,7 @@ const resgisterUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -47,7 +45,6 @@ const resgisterUser = asyncHandler(async (req, res) => {
 // @access   Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-
   //check for user email
   const user = await User.findOne({ email });
   if (user && (await bcrypt.compare(password, user.password))) {
@@ -55,6 +52,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -70,6 +68,13 @@ const getMe = asyncHandler(async (req, res) => {
     message: 'User data display',
   });
 });
+
+//generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
 
 module.exports = {
   resgisterUser,
