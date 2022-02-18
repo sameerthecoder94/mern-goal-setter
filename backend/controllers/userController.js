99a5f44ee7941733946e8671f10aa8c9e10efba6
sameerthecoder94 <sameerthecoder@gmail.com>
2022-02-18 10:17:41 -0500

    register and login user setup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,18 +7,59 @@ const User = require('../models/userModal');
 // @route    POST /api/users
 // @access   Public
 const resgisterUser = asyncHandler(async (req, res) => {
-  res.json({
-    message: 'Register User',
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please add all fields');
+  }
+
+  //check if user exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  //hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
   });
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // @desc     Authenticate a user
 // @route    POST /api/users/login
 // @access   Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({
-    message: 'Login User',
-  });
+  const { email, password } = req.body;
+
+  //check for user email
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid credentials!');
+  }
 });
 
 // @desc     Get user data
