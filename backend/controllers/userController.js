2156c71d29e8f758041ca16c3fecc016be7b90ca
sameerthecoder94 <sameerthecoder@gmail.com>
2022-02-18 10:49:29 -0500

    created authMiddleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -62,7 +62,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // @desc     Get user data
 // @route    GET /api/users/me
-// @access   Public
+// @access   Private
 const getMe = asyncHandler(async (req, res) => {
   res.json({
     message: 'User data display',
